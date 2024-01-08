5730b83db806c5e49894833e8d8afd9739876033
Sam Zack <szack25@cgps.org>
2024-01-08 14:30:00 -0500

    added post  tag function script to gulp compiler

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,8 @@ function js(done) {
             'node_modules/@tryghost/shared-theme-assets/assets/js/v1/lib/**/*.js',
             'node_modules/@tryghost/shared-theme-assets/assets/js/v1/main.js',
             'assets/js/lib/*.js',
-            'assets/js/main.js'
+            'assets/js/main.js',
+            'assets/js/post-tag-add.js'
         ], {sourcemaps: true}),
         concat('main.min.js'),
         uglify(),
